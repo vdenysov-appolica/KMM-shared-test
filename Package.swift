2ef86f2e14f13521422d84f22a638a2892674653
runner <runner@Mac-1717500380846.local>
2024-06-04 12:06:05 +0000

    KMM SPM package release for 2.0.0

--- Package.swift
+++ Package.swift
@@ -1,7 +1,11 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://maven.pkg.github.com/vdenysov-appolica/KMM-shared-test/co/volodymyrdenysov/kmmtest/orion-kmmbridge/2.0.0/orion-kmmbridge-2.0.0.zip"
+let remoteKotlinChecksum = "f7f3994004d9e427b75f7d6c1e4dacacffffc0d3650e97f56e4ece832a9a62e6"
 let packageName = "orion"
+// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
@@ -17,7 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./allshared/build/XCFrameworks/debug/\(packageName).xcframework"
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
         ,
     ]
